7ec2e383bdacb068bc5ce841e0dd8a5b291079c8
Daniel <d15_1_89@msn.com>
2023-12-14 18:03:22 +0100

    contact

--- lib/pages/options.dart
+++ lib/pages/options.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_demo/classes/loading.dart';
 import 'package:flutter_demo/locales.dart';
 import 'package:flutter_demo/main.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
 import 'package:flutter_translate/flutter_translate.dart';
 import 'package:unicons/unicons.dart';
 
 class OptionsPage extends StatefulWidget {
-  const OptionsPage({Key? key}) : super(key: key);
+  const OptionsPage({super.key});
   @override
   OptionsPageState createState() => OptionsPageState();
 }
@@ -59,9 +60,28 @@ class OptionsPageState extends State<OptionsPage> {
                 },
               ),
             ),
+            ListTile(
+              leading: const Icon(UniconsLine.envelope_alt),
+              title: Text(translate('options.contact_me')),
+              // subtitle: Text(translate('options.contact_subtitle')),
+              trailing: const Icon(UniconsLine.arrow_right),
+              onTap: () async {
+                contact();
+              },
+            ),
           ],
         ).toList(),
       ),
     );
+  }
+
+  Future<void> contact() async {
+    final Email newEmail = Email(
+      body: '',
+      subject: "Hi Daniel!",
+      recipients: ["d15_1_89@msn.com"],
+      isHTML: false,
+    );
+    await FlutterEmailSender.send(newEmail);
   }
 }
